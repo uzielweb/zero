14f28a2350aa95bf33b792ea5bb3623aa78e52a5
Uziel Almeida Oliveira <uzielweb@gmail.com>
2019-03-19 04:50:04 -0300

    Fix to custom width Boostratp sizes approach

--- html/modules.php
+++ html/modules.php
@@ -34,6 +34,9 @@ $headerTag      = htmlspecialchars($params->get('header_tag', 'h3'), ENT_QUOTES,
 $bootstrapSize  = (int) $params->get('bootstrap_size', 0);
 $moduleClass    = $bootstrapSize !== 0 ? ' '. $col_bootversion . $bootstrapSize : '';
 if ($template->params->get('type_of_layout') == 'custom') {
+  if($bootstrapSize == "0"){
+    $bootstrapSize = '12';
+  }
 $col_module_width = ' style="float:left; width:'.(100/12*$bootstrapSize).'%;"';
 }
 else{
@@ -43,14 +46,12 @@ $col_module_width = '';
 $headerClass    = $params->get('header_class');
 $headerClass    = !empty($headerClass) ? ' class="module-title ' . htmlspecialchars($headerClass, ENT_COMPAT, 'UTF-8') . '"' : ' class="module-title"';
 if (!empty ($module->content)) : ?>
-<<?php echo $moduleTag; ?> class="moduletable <?php echo $module->name." ".$module->name."-".$module->id." ".$module->name."-".$module->position.htmlspecialchars($params->get('moduleclass_sfx'), ENT_COMPAT, 'UTF-8') . $moduleClass; ?>"<?php echo $col_module_width?>>
-<?php if ((bool) $module->showtitle) :?>
-<<?php echo $headerTag . $headerClass . '>' . $module->title; ?>
-</<?php echo $headerTag; ?>>
-<?php endif; ?>
-<?php echo $module->content; ?>
-</<?php echo $moduleTag; ?>>
-<?php endif;
+<<?php echo $moduleTag; ?>
+    class="moduletable <?php echo $module->name." ".$module->name."-".$module->id." ".$module->name."-".$module->position.htmlspecialchars($params->get('moduleclass_sfx'), ENT_COMPAT, 'UTF-8') . $moduleClass; ?>"
+    <?php echo $col_module_width?>>
+    <?php if ((bool) $module->showtitle) :?>
+    <<?php echo $headerTag . $headerClass . '>' . $module->title; ?> </<?php echo $headerTag; ?>> <?php endif; ?>
+        <?php echo $module->content; ?> </<?php echo $moduleTag; ?>> <?php endif;
 }
 /*
 * html5 (chosen html5 tag and font header tags)
@@ -77,6 +78,9 @@ $headerTag      = htmlspecialchars($params->get('header_tag', 'h3'), ENT_QUOTES,
 $bootstrapSize  = (int) $params->get('bootstrap_size', 0);
 $moduleClass    = $bootstrapSize !== 0 ? ' '.$col_bootversion . $bootstrapSize : '';
 if ($template->params->get('type_of_layout') == 'custom') {
+  if($bootstrapSize == "0"){
+    $bootstrapSize = '12';
+  }
 $col_module_width = ' style="float:left; width:'.(100/12*$bootstrapSize).'%;"';
 }
 else{
@@ -85,15 +89,12 @@ $col_module_width = '';
 // Temporarily store header class in variable
 $headerClass    = $params->get('header_class');
 $headerClass    = !empty($headerClass) ? ' class="module-title ' . htmlspecialchars($headerClass, ENT_COMPAT, 'UTF-8') . '"' : ' class="module-title"';
-if (!empty ($module->content)) : ?>
-<<?php echo $moduleTag; ?> class="moduletable <?php echo " module-".$module->name." ".$module->name."-".$module->id." ".$module->name."-".$module->position.htmlspecialchars($params->get('moduleclass_sfx'), ENT_COMPAT, 'UTF-8') . $moduleClass; ?>"<?php echo $col_module_width?>>
-<?php if ((bool) $module->showtitle) :?>
-<<?php echo $headerTag . $headerClass . '>' . $module->title; ?>
-</<?php echo $headerTag; ?>>
-<?php endif; ?>
-<?php echo $module->content; ?>
-</<?php echo $moduleTag; ?>>
-<?php endif;
+if (!empty ($module->content)) : ?> <<?php echo $moduleTag; ?>
+        class="moduletable <?php echo " module-".$module->name." ".$module->name."-".$module->id." ".$module->name."-".$module->position.htmlspecialchars($params->get('moduleclass_sfx'), ENT_COMPAT, 'UTF-8') . $moduleClass; ?>"
+        <?php echo $col_module_width?>>
+        <?php if ((bool) $module->showtitle) :?>
+        <<?php echo $headerTag . $headerClass . '>' . $module->title; ?> </<?php echo $headerTag; ?>> <?php endif; ?>
+            <?php echo $module->content; ?> </<?php echo $moduleTag; ?>> <?php endif;
 }
 /*
 * Module chrome that wraps the module in a table
@@ -118,28 +119,32 @@ $col_bootversion = 'col-md-';
 $bootstrapSize  = (int) $params->get('bootstrap_size', 0);
 $moduleClass    = $bootstrapSize !== 0 ? ' '.$col_bootversion . $bootstrapSize : '';
 if ($template->params->get('type_of_layout') == 'custom') {
+  if($bootstrapSize == "0"){
+    $bootstrapSize = '12';
+  }
 $col_module_width = ' style="float:left; width:'.(100/12*$bootstrapSize).'%;"';
 }
 else{
 $col_module_width = '';
 }
 if (!empty ($module->content)) :
-?>
-<table cellpadding="0" cellspacing="0" class="moduletable <?php echo " module-".$module->name." ".$module->name."-".$module->id." ".$module->name."-".$module->position.htmlspecialchars($params->get('moduleclass_sfx'), ENT_COMPAT, 'UTF-8'); ?><?php echo $moduleClass;?>"<?php echo $col_module_width; ?>>
-<?php if ((bool) $module->showtitle) : ?>
-<tr>
-  <th>
-    <?php echo $module->title; ?>
-  </th>
-</tr>
-<?php endif; ?>
-<tr>
-  <td>
-    <?php echo $module->content; ?>
-  </td>
-</tr>
-</table>
-<?php  endif;
+?> <table cellpadding="0" cellspacing="0"
+            class="moduletable <?php echo " module-".$module->name." ".$module->name."-".$module->id." ".$module->name."-".$module->position.htmlspecialchars($params->get('moduleclass_sfx'), ENT_COMPAT, 'UTF-8'); ?><?php echo $moduleClass;?>"
+            <?php echo $col_module_width; ?>>
+            <?php if ((bool) $module->showtitle) : ?>
+            <tr>
+                <th>
+                    <?php echo $module->title; ?>
+                </th>
+            </tr>
+            <?php endif; ?>
+            <tr>
+                <td>
+                    <?php echo $module->content; ?>
+                </td>
+            </tr>
+            </table>
+            <?php  endif;
 }
 /*
 * Module chrome that wraps the tabled module output in a <td> tag of another table
@@ -148,14 +153,14 @@ function modChrome_zero_horz($module, &$params, &$attribs)
 {
 if (!empty ($module->content)) :
 ?>
-<table cellspacing="1" cellpadding="0" width="100%">
-  <tr>
-    <td>
-      <?php modChrome_zero_table($module, $params, $attribs); ?>
-    </td>
-  </tr>
-</table>
-<?php endif;
+            <table cellspacing="1" cellpadding="0" width="100%">
+                <tr>
+                    <td>
+                        <?php modChrome_zero_table($module, $params, $attribs); ?>
+                    </td>
+                </tr>
+            </table>
+            <?php endif;
 }
 /*
 * xhtml (divs and font header tags)
@@ -178,11 +183,15 @@ if ($bootstrap_version == '4'){
 $col_bootversion = 'col-md-';
 }
 }
+
 $moduleTag      = htmlspecialchars($params->get('module_tag', 'div'), ENT_QUOTES, 'UTF-8');
 $headerTag      = htmlspecialchars($params->get('header_tag', 'h3'), ENT_QUOTES, 'UTF-8');
 $bootstrapSize  = (int) $params->get('bootstrap_size', 0);
 $moduleClass    = $bootstrapSize !== 0 ? ' '.$col_bootversion . $bootstrapSize : '';
 if ($template->params->get('type_of_layout') == 'custom') {
+  if($bootstrapSize == "0"){
+    $bootstrapSize = '12';
+  }
 $col_module_width = ' style="float:left; width:'.(100/12*$bootstrapSize).'%;"';
 }
 else{
@@ -192,14 +201,12 @@ $col_module_width = '';
 $headerClass    = $params->get('header_class');
 $headerClass    = $headerClass ? ' class="module-title ' . htmlspecialchars($headerClass, ENT_COMPAT, 'UTF-8') . '"' : ' class="module-title"';
 if (!empty ($module->content)) : ?>
-<<?php echo $moduleTag; ?> class="moduletable <?php echo " module-".$module->name." ".$module->name."-".$module->id." ".$module->name."-".$module->position.htmlspecialchars($params->get('moduleclass_sfx'), ENT_COMPAT, 'UTF-8') . $moduleClass; ?>"<?php echo $col_module_width?>>
-<?php if ((bool) $module->showtitle) : ?>
-<<?php echo $headerTag . $headerClass . '>' . $module->title; ?>
-</<?php echo $headerTag; ?>>
-<?php endif; ?>
-<?php echo $module->content; ?>
-</<?php echo $moduleTag; ?>>
-<?php endif;
+            <<?php echo $moduleTag; ?>
+                class="moduletable <?php echo " module-".$module->name." ".$module->name."-".$module->id." ".$module->name."-".$module->position.htmlspecialchars($params->get('moduleclass_sfx'), ENT_COMPAT, 'UTF-8') . $moduleClass; ?>"
+                <?php echo $col_module_width?>>
+                <?php if ((bool) $module->showtitle) : ?>
+                <<?php echo $headerTag . $headerClass . '>' . $module->title; ?> </<?php echo $headerTag; ?>>
+                    <?php endif; ?> <?php echo $module->content; ?> </<?php echo $moduleTag; ?>> <?php endif;
 }
 /*
 * Module chrome that allows for rounded corners by wrapping in nested div tags
@@ -224,26 +231,29 @@ $col_bootversion = 'col-md-';
 $bootstrapSize  = (int) $params->get('bootstrap_size', 0);
 $moduleClass    = $bootstrapSize !== 0 ? ' '.$col_bootversion . $bootstrapSize : '';
 if ($template->params->get('type_of_layout') == 'custom') {
+  if($bootstrapSize == "0"){
+    $bootstrapSize = '12';
+  }
 $col_module_width = ' style="float:left; width:'.(100/12*$bootstrapSize).'%;"';
 }
 else{
 $col_module_width = '';
 }
 if (!empty ($module->content)) :
-?>
-<div class="module <?php echo " module-".$module->name." ".$module->name."-".$module->id." ".$module->name."-".$module->position.htmlspecialchars($params->get('moduleclass_sfx'), ENT_COMPAT, 'UTF-8') . $moduleClass; ?>"<?php echo $col_module_width; ?>>
-<div>
-  <div>
-    <div>
-      <?php if ((bool) $module->showtitle) : ?>
-      <h3>
-        <?php echo $module->title; ?>
-      </h3>
-      <?php endif; ?>
-      <?php echo $module->content; ?>
-    </div>
-  </div>
-</div>
-</div>
-<?php  endif;
+?> <div class="module <?php echo " module-".$module->name." ".$module->name."-".$module->id." ".$module->name."-".$module->position.htmlspecialchars($params->get('moduleclass_sfx'), ENT_COMPAT, 'UTF-8') . $moduleClass; ?>"
+                    <?php echo $col_module_width; ?>>
+                    <div>
+                        <div>
+                            <div>
+                                <?php if ((bool) $module->showtitle) : ?>
+                                <h3>
+                                    <?php echo $module->title; ?>
+                                </h3>
+                                <?php endif; ?>
+                                <?php echo $module->content; ?>
+                            </div>
+                        </div>
+                    </div>
+                    </div>
+                    <?php  endif;
 }
