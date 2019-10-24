dc3213c1eaaf5851361ae3309ac997a7b409df37
Uziel Almeida Oliveira <uzielweb@gmail.com>
2019-10-24 13:57:12 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,46 +1,40 @@
+jQuery(function($) {
 
-jQuery(function ($) {
+    //caches a jQuery object containing the header element
+    var header = $("#tppa-header");
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop();
 
-     //caches a jQuery object containing the header element
-     var header = $("#tppa-header");
-     $(window).scroll(function() {
-         var scroll = $(window).scrollTop();
- 
-         if (scroll >= 90) {
-             header.removeClass('stickyheader').addClass("stickedheader");
-         } else {
-             header.removeClass("stickedheader").addClass('stickyheader');
-         }
-     });
-     var scrolltoTopButton = $('#scrolltoTopButton');
+        if (scroll >= 90) {
+            header.removeClass('stickyheader').addClass("stickedheader");
+        } else {
+            header.removeClass("stickedheader").addClass('stickyheader');
+        }
+    });
+    var scrolltoTopButton = $('#scrolltoTopButton');
 
-     $(window).scroll(function() {
-       if ($(window).scrollTop() > 300) {
-        scrolltoTopButton.addClass('show');
-       } else {
-        scrolltoTopButton.removeClass('show');
-       }
-     });
-   
-     scrolltoTopButton.on('click', function(e) {
-       e.preventDefault();
-       $('html, body').animate({scrollTop:0}, '300');
-     });
-   
+    $(window).scroll(function() {
+        if ($(window).scrollTop() > 300) {
+            scrolltoTopButton.addClass('show').fadeIn();
+        } else {
+            scrolltoTopButton.removeClass('show').fadeOut();
+        }
+    });
 
-  
-   $('#offcanvas_button').click(function(){
-    $('.onCanvas').toggleClass('showmenu');
-    $('.offCanvas').toggleClass('showoffcanvas');
-   }        
-   );
-   $('#close-canvas').click(function(){
-    $('.onCanvas').toggleClass('showmenu');
-    $('.offCanvas').toggleClass('showoffcanvas');
-   }        
-   );
+    scrolltoTopButton.on('click', function(e) {
+        e.preventDefault();
+        $('html, body').animate({ scrollTop: 0 }, '300');
+    });
+
+
+
+    $('#offcanvas_button').click(function() {
+        $('.onCanvas').toggleClass('showmenu');
+        $('.offCanvas').toggleClass('showoffcanvas');
+    });
+    $('#close-canvas').click(function() {
+        $('.onCanvas').toggleClass('showmenu');
+        $('.offCanvas').toggleClass('showoffcanvas');
+    });
 
 });
-
-
-
