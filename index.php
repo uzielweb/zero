0f9de4c4f2af70459e7879bc0e7bde46126a832f
Uziel <uziel.oliveira@intercode.com.br>
2023-03-14 13:48:14 -0300

    Start

---
+++ index.php
@@ -0,0 +1,106 @@
+<?php
+/*
+ * @package     Joomla.Site
+ * @subpackage  Templates.zero
+ *
+ * @copyright   Copyright (C) 2018 Uziel Almeida Oliveira via Ponto Mega, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+defined('_JEXEC') or die('Restricted access');
+include_once JPATH_THEMES . '/' . $this->template . '/logic.php';
+?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>">
+<head>
+    <jdoc:include type="head" />
+    <?php echo $customheadercode; ?>
+</head>
+<body class="<?php echo $bodyClass; ?>">
+<?php if ($this->countModules('logo') || $this->countModules('menu') || $this->countModules('search')): ?>
+    <header class="header">
+        <div class="container<?php echo $fluid; ?>">
+            <div class="row">
+                <div class="col-12">
+                    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                        <div class="container-fluid">
+                            <a class="navbar-brand" href="<?php echo $this->baseurl; ?>/">
+                                <?php if ($logo) : ?>
+                                    <img src="<?php echo $logo; ?>" alt="<?php echo $sitename; ?>" width="<?php echo $logowidth; ?>" height="<?php echo $logoheight; ?>" />
+                                <?php else : ?>
+                                    <?php echo htmlspecialchars($sitename); ?>
+                                <?php endif; ?>
+                            </a>
+                            <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
+                                <span class="navbar-toggler-icon"></span>
+                            </button>
+                            <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+                                <div class="offcanvas-header">
+                                    <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Offcanvas</h5>
+                                    <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                                </div>
+                                <div class="offcanvas-body">
+                                    <jdoc:include type="modules" name="menu" style="none" />
+                                </div>
+                            </div>
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
+<?php endif; ?>
+    <main class="main">
+        <div class="container<?php echo $fluid; ?>">
+            <div class="row">
+                <?php if ($this->countModules('main-top')): ?>
+                    <?php echo  positions(array('main-top' => 12), $this->template . '_xhtml'); ?>
+                <?php endif; ?>
+                <?php if ($this->countModules('breadcrumbs')): ?>
+                    <div class="col-12">
+                        <jdoc:include type="modules" name="breadcrumbs" style="none" />
+                    </div>
+                <?php endif; ?>
+                <?php if ($this->countModules('left')):?>
+                    <div class="col-12 <?php echo $default_column.$col_side;?>">
+                        <jdoc:include type="modules" name="left" style="none" />
+                    </div>
+                <?php endif; ?>
+                <div class="col-12 <?php echo $default_column ?>">
+                <?php if ($this->countModules('content-top')): ?>
+                    <div class="row">
+                    <?php echo  positions(array('content-top' => 12), $this->template . '_xhtml'); ?>
+                    </div>
+                <?php endif; ?>
+                    <jdoc:include type="message" />
+                    <jdoc:include type="component" />
+                <?php if ($this->countModules('content-bottom')): ?>
+                    <div class="row">
+                        <?php echo  positions(array('content-bottom' => 12), $this->template . '_xhtml'); ?>
+                    </div>                    
+                <?php endif; ?>
+                </div>
+                <?php if ($this->countModules('right')):?>
+                    <div class="col-12 <?php echo $default_column.$col_side;?>">
+                        <jdoc:include type="modules" name="right" style="none" />
+                    </div>
+                <?php endif; ?>
+                <?php if ($this->countModules('main-bottom')): ?>
+                    <?php echo  positions(array('main-bottom' => 12), $this->template . '_xhtml'); ?>
+                <?php endif; ?>
+            </div>
+        </div>
+    </main>
+    <?php if ($this->countModules('footer')): ?>
+        <footer class="footer">
+            <div class="container<?php echo $fluid; ?>">
+                <div class="row">
+                    <?php echo  positions(array('footer' => 12), $this->template . '_xhtml'); ?>
+                </div>
+            </div>
+        </footer>
+    <?php endif; ?>
+    
+    <?php echo $customafterbodycode; ?>
+    <?php echo $customfootercode; ?>
+</body>
+</html>
---
+++ logic.php
@@ -0,0 +1,259 @@
+<?php
+// variables
+use Joomla\CMS\Factory;
+use Joomla\CMS\Uri\Uri;
+$app = Factory::getApplication();
+$doc = $app->getDocument();
+$template = $app->getTemplate(true);
+$headData = $doc->getHeadData();
+$params = $template->params;
+$logo = $params->get('logo', '');
+$logosize = $logo ? getimagesize(JUri::base('true') . $logo) : '';
+$logowidth = $logosize ? $logosize[0] : '';
+$logoheight = $logosize ? $logosize[1] : '';
+$favicon = $params->get('favicon', '');
+$type_of_layout = $params->get('type_of_layout', 'custom');
+$default_column = $params->get('default_column', 'col-lg');
+$proportional_equal = $params->get('proportional_equal', 'equal');
+$col_side = $params->get('col_side', '-3');
+$animate_css_from_template = $params->get('animate_css_from_template', '1');
+$fontawesome = $params->get('fontawesome', 'css_from_template');
+$jquery_from_template = $params->get('jquery_from_template', '1');
+$bootstrap_from_template = $params->get('bootstrap_from_template', '1');
+$default_image_for_article = $params->get('default_image_for_article', '');
+$default_image_for_category = $params->get('default_image_for_category', '');
+$fluid = $params->get('-fluid', '');
+$sections = $params->get('sections', '');
+$customheadercode = $params->get('customheadercode', '');
+$customafterbodycode = $params->get('customafterbodycode', '');
+$customfootercode = $params->get('customfootercode', '');
+$customcss = $params->get('customcss', '');
+$opengraph_enabled = $params->get('opengraph_enabled', '1');
+$default_opengraph_image = $params->get('default_opengraph_image', '');
+$view = $app->input->get('view');
+$layout = $app->input->get('layout');
+$task = $app->input->get('task');
+$itemid = $app->input->get('Itemid');
+$option = $app->input->get('option');
+$pageclass = $app->input->get('pageclass_sfx');
+$active = $app->getMenu()->getActive();
+$ishome = ($active == $app->getMenu()->getDefault()) ? 'is-home' : 'not-home';
+$language = Factory::getLanguage();
+$langtag = $language->getTag();
+$bodyClass = $ishome . ' language-'.$this->language. ' option-' . str_replace('com_', '', $option) . ' view-' . $view . ' layout-' . ($layout ? $layout : 'default') . ' task-' . ($task ? $task : 'default') . ' itemid-' . $itemid . $pageclass;
+$sitename = $app->get('sitename');
+$doc->setMetaData('viewport', 'width=device-width, initial-scale=1.0, maximum-scale=6.0, user-scalable=1');
+if (file_exists(JPATH_THEMES . '/' . $this->template . '/css/bootstrap.min.css') && $bootstrap_from_template == 1 && $type_of_layout == 'bootstrap') {
+    $doc->addStyleSheet(Uri::base('true') . 'templates/' . $this->template . '/css/bootstrap.min.css');
+    $doc->addScript(Uri::base('true') . 'templates/' . $this->template . '/js/bootstrap.bundle.min.js');
+}
+if (file_exists(JPATH_THEMES . '/' . $this->template . '/css/all.min.css') && $fontawesome == 'css_from_template') {
+    $doc->addStyleSheet(Uri::base('true') . 'templates/' . $this->template . '/css/all.min.css');
+}
+if (file_exists(JPATH_THEMES . '/' . $this->template . '/css/animate.min.css') && $animate_css_from_template == 1) {
+    $doc->addStyleSheet(Uri::base('true') . 'templates/' . $this->template . '/css/animate.min.css');
+}
+if (file_exists(JPATH_THEMES . '/' . $this->template . '/css/template.css')) {
+    $doc->addStyleSheet(Uri::base('true') . 'templates/' . $this->template . '/css/template.css');
+}
+if (file_exists(JPATH_THEMES . '/' . $this->template . '/css/custom.css')) {
+    $doc->addStyleSheet(Uri::base('true') . 'templates/' . $this->template . '/css/custom.css');
+}
+if (file_exists(JPATH_THEMES . '/' . $this->template . '/js/jquery.min.js') && $jquery_from_template == 1) {
+    $doc->addScript(Uri::base('true') . 'templates/' . $this->template . '/js/jquery.min.js');
+}
+if (file_exists(JPATH_THEMES . '/' . $this->template . '/js/template.js')) {
+    $doc->addScript(Uri::base('true') . 'templates/' . $this->template . '/js/template.js');
+}
+if (file_exists(JPATH_THEMES . '/' . $this->template . '/js/custom.js')) {
+    $doc->addScript(Uri::base('true') . 'templates/' . $this->template . '/js/custom.js');
+}
+if ($customcss) {
+    $doc->addStyleDeclaration($customcss);
+}
+if ($opengraph_enabled == 1) {
+    $doc->setMetaData('og:title', $doc->getTitle());
+    $doc->setMetaData('og:type', 'website');
+    $doc->setMetaData('og:url', Uri::current());
+    $doc->setMetaData('og:site_name', $app->get('sitename'));
+    $doc->setMetaData('og:description', $doc->getDescription());
+    // twitter cards
+    $doc->setMetaData('twitter:card', 'summary');
+    $doc->setMetaData('twitter:site', $app->get('sitename'));
+    $doc->setMetaData('twitter:title', $doc->getTitle());
+    $doc->setMetaData('twitter:description', $doc->getDescription());
+    $doc->setMetaData('twitter:creator', $app->get('sitename'));
+    if ($default_opengraph_image) {
+        $doc->setMetaData('og:image', Uri::base('true') . $default_opengraph_image);
+        $doc->setMetaData('twitter:image', Uri::base('true') . $default_opengraph_image);
+    }
+    // check if view is article or category
+    if ($view == 'article') {
+        $doc->setMetaData('og:image', Uri::base('true') . $default_image_for_article);
+        $doc->setMetaData('twitter:image', Uri::base('true') . $default_image_for_article);
+        $article_id = $app->input->get('id');
+        $db = Factory::getDbo();
+        $query = $db->getQuery(true);
+        $query->select($db->quoteName(array('images')));
+        $query->from($db->quoteName('#__content'));
+        $query->where($db->quoteName('id') . ' = ' . $db->quote($article_id));
+        $db->setQuery($query);
+        $result = $db->loadObject();
+        $images = json_decode($result->images);
+        // check image intro, fulltext, or in article content and choose one of them
+        if ($images->image_intro) {
+            $doc->setMetaData('og:image', Uri::base('true') . $images->image_intro);
+            $doc->setMetaData('twitter:image', Uri::base('true') . $images->image_intro);
+        } elseif ($images->image_fulltext) {
+            $doc->setMetaData('og:image', Uri::base('true') . $images->image_fulltext);
+            $doc->setMetaData('twitter:image', Uri::base('true') . $images->image_fulltext);
+        } else {
+            $article = JTable::getInstance('content');
+            $article->load($article_id);
+            $content = $article->introtext . $article->fulltext;
+            $regex = '/<img.+src=[\'"]([^\'"]+)[\'"].*>/i';
+            preg_match($regex, $content, $matches);
+            if (isset($matches[1])) {
+                $doc->setMetaData('og:image', $matches[1]);
+                $doc->setMetaData('twitter:image', $matches[1]);
+            }
+        }
+    }
+    if ($option = 'com_content' && $view == 'category') {
+        $doc->setMetaData('og:image', Uri::base('true') . $default_image_for_category);
+        $doc->setMetaData('twitter:image', Uri::base('true') . $default_image_for_category);
+        $category_id = $app->input->get('id');
+        $db = Factory::getDbo();
+        $query = $db->getQuery(true);
+        $query->select($db->quoteName(array('params')));
+        $query->from($db->quoteName('#__categories'));
+        $query->where($db->quoteName('id') . ' = ' . $db->quote($category_id));
+        $db->setQuery($query);
+        $result = $db->loadObject();
+        $category_params = json_decode($result->params);
+        if ($category_params->image) {
+            $doc->setMetaData('og:image', Uri::base('true') . $category_params->image);
+            $doc->setMetaData('twitter:image', Uri::base('true') . $category_params->image);
+        }
+    }
+}
+if ($favicon) {
+    $doc->addFavicon(Uri::base('true') . $favicon);
+}
+// remove generator
+$this->setGenerator(null);
+
+//Sections Custom StyleSheet. Please configure in Joomla Template Administration as you need
+$sections = $template->params->get('sections', '');
+if ($sections) {
+    foreach ($sections as $skey => $item) {
+        //$item->section;
+        //$item->section_background;
+        //$item->section_background_color;
+        //$item->section_padding;
+        //$item->section_margin;
+        $item_background = $item->section_background == '' ? '' : 'background-image: url(\'' . Uri::base('true') . $item->section_background . '\');';
+        $item_background_color = $item->section_background_color == '' ? '' : 'background-color: ' . $item->section_background_color . ';';
+        $item_padding = $item->section_padding == '' ? '' : 'padding: ' . $item->section_padding . ';';
+        $item_margin = $item->section_margin == '' ? '' : 'margin: ' . $item->section_margin . ';';
+        $doc->addStyleDeclaration('
+      #' . $this->template . '-' . $item->section . '{'
+            . $item_background
+            . $item_background_color
+            . $item_padding
+            . $item_margin . '
+      }
+        ');
+    }
+}
+
+function positions($position, $style)
+{
+    $app = Factory::getApplication('site');
+    $template = $app->getTemplate(true);
+    $defaultmode = $template->params->get('default_mode', 'bootstrap');
+    $default_column = $template->params->get('default_column ', 'col-lg-');
+    // Default width - for one column
+    $col_middle_boot_width = $default_column . '12';
+    // This gets new value, if there is more than one active position
+    // For Bootstrap use
+    if ($defaultmode == "bootstrap") {
+        $width = $default_column . '12';
+    }
+    // For Custom width use
+    if ($defaultmode == "custom") {
+        $width = "100";
+    }
+    // Number of positions, which have modules
+    $countOfActivePositions = 0;
+    // Positions to search modules in
+    // Loop over every position
+    $totalWidth = 0;
+    foreach ($position as $name => $value) {
+        // If position has modules
+        if (Factory::getDocument()->countModules($name)) {
+            // Increase active positions count
+            $countOfActivePositions++;
+            $totalWidth = $totalWidth + $value;
+        }
+    }
+    if ($countOfActivePositions > 0) {
+        // For Bootstrap with equal widths use
+        if ((Factory::getDocument()->params->get('type_of_layout') == "bootstrap") and (Factory::getDocument()->params->get('proportional_equal') == "equal")) {
+            $width = $default_column . (12 / $countOfActivePositions);
+        }
+        // For Custom with equal widths use
+        if ((Factory::getDocument()->params->get('type_of_layout') == "custom") and (Factory::getDocument()->params->get('proportional_equal') == "equal")) {
+            $width = (100 / $countOfActivePositions);
+        }
+    }
+    foreach ($position as $name => $value) {
+        if ($totalWidth < 100) {
+            // For Bootstrap remove / comment the $width bellow
+            // For custom with equal widths add / uncomment the $width bellow
+            // For custom with proportional widths add / uncomment the $width bellow
+            if ((Factory::getDocument()->params->get('type_of_layout') == "custom") and (Factory::getDocument()->params->get('proportional_equal') == "proportional")) {
+                $width = round($value * 100 / $totalWidth);
+            }
+            // For Bootstrap with proportional widths add / uncomment the $width bellow
+            // For Bootstrap with equal widths remove / comment the $width bellow
+            if ((Factory::getDocument()->params->get('type_of_layout') == "bootstrap") and (Factory::getDocument()->params->get('proportional_equal') == "proportional")) {
+                $width = $default_column . round($value * 12 / $totalWidth);
+            }
+            if ((Factory::getDocument()->params->get('type_of_layout') == "custom") and (Factory::getDocument()->params->get('proportional_equal') == "equal")) {
+                // For Bootstrap with proportional widths remove / comment the IF bellow
+                // For Custom with with more then 1 position add / uncomment the IF bellow
+                if (($value > 0) and ($countOfActivePositions == 2) and (count($position) > 2)) {
+                    $width = 100 / 2;
+                }
+                // For Bootstrap with proportional widths remove / comment the IF bellow
+                // For Custom with with more then 1 position add / uncomment the IF bellow
+                if (($value > 0) and ($countOfActivePositions == 3) and (count($position) > 3)) {
+                    $width = 100 / 3;
+                }
+                // For Bootstrap with proportional widths remove / comment the IF bellow
+                // For Custom with with more then 1 position add / uncomment the IF bellow
+                if (($value > 0) and ($countOfActivePositions == 4) and (count($position) > 4)) {
+                    $width = 100 / 4;
+                }
+            }
+        }
+        if (Factory::getDocument()->countModules($name)) {
+            // For Bootstrap use
+            if (Factory::getDocument()->params->get('type_of_layout') == "bootstrap") {
+                echo '<div class="' . $name . ' ' . $width . '"><jdoc:include type="modules" name="' . $name . '" style="' . $style . '" /></div>';
+            }
+            // For Custom Width use
+            if (Factory::getDocument()->params->get('type_of_layout') == "custom") {
+                echo '<div class="' . $name . ' ' . '" style="width:' . $width . '%"><jdoc:include type="modules" name="' . $name . '" style="' . $style . '" /></div>';
+            }
+        }
+    }
+}
+// For Bootstrap use the grid divisions by 12 in the width keys like this: 1,2,3,4,5,6,7,8,9,10,11
+// Then use the code like this bellow
+// echo positions(array('menu' => 4, 'login' => 6, 'nada' => 2), 'block');
+// If You wish to use with equal widths FOREVER use the code like this bellow
+// echo positions(array('menu', 'login', 'nada'), 'block');
+// If You wish to use with custom percentages use the code like this bellow
+// echo positions(array('menu' => 60, 'login' => 20, 'nada' => 20), 'block');
